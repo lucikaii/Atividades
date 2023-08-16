779501d88291efbf6970c85a3c715b5285f64a40
Bragakaii <bragakaisenai@gmail.com>
2023-08-16 17:01:23 -0300

    media

---
+++ media.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const botaoMedia = document.getElementById('botaoMedia')
+const bimUm = Number (document.getElementById('bimUm').value)
+const bimDois = Number (document.getElementById('bimDois').value)
+const bimTres = Number (document.getElementById('bimTres').value)
+const bimQuatro = Number (document.getElementById('bimQuatro').value)
+const mediafinal = document.getElementById('media')
+
+function somarMedia(){
+ const media = (bimUm + bimDois + bimTres + bimQuatro)/4
+ mediafinal.textContent = media
+}
+
+
+botaoMedia.addEventListener('click', somarMedia)
+
